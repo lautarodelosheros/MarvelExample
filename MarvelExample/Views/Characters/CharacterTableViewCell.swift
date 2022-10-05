4f268f21879262f9d45712fd5caeb77bb6fec82d
Lautaro de los Heros <lautarodelosheros@gmail.com>
2022-10-05 16:36:59 -0300

    Add CharacterTableViewCell

---
+++ MarvelExample/Views/Characters/CharacterTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CharacterTableViewCell.swift
+//  MarvelExample
+//
+//  Created by Lautaro de los Heros on 05/10/2022.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
