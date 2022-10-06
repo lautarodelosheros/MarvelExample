528cad5e9774fdb8efbd62ddce9c861ab6558457
Lautaro de los Heros <lautarodelosheros@gmail.com>
2022-10-06 20:36:25 -0300

    CharacterTableViewCell - Add shadow

--- MarvelExample/Views/Characters/CharacterTableViewCell.swift
+++ MarvelExample/Views/Characters/CharacterTableViewCell.swift
@@ -9,12 +9,23 @@ import UIKit
 
 class CharacterTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var shadowView: UIView!
     @IBOutlet weak var characterImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUpShadowView()
+    }
+    
+    private func setUpShadowView() {
+        shadowView.layer.cornerRadius = 4
+        shadowView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOpacity = 0.2
+        shadowView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        shadowView.layer.shadowRadius = 4
     }
     
     func bind(with character: Character) {
